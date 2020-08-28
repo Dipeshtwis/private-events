920ee0b6f40558b83ac98c9c5f1f74fceec3d6ea
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-28 15:32:14 +0530

    create event model and association between user and event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :events, foreign_key: :creator_id, class_name: 'Event'
   validates :name, presence: true, uniqueness: { case_sensitive: false },
                    length: { minimum: 4, maximum: 25 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_170754) do
+ActiveRecord::Schema.define(version: 2020_08_28_095421) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.datetime "date"
+    t.integer "creator_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
