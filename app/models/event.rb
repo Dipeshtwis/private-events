e405e21c5f52ee389f96580a94da12820f4a9e04
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-28 21:33:19 +0530

    Generate Invitation model and create association with has_many through

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :invitations, foreign_key: :attended_event_id
+  has_many :attendees, through: :invitations, source: :attendee
+
+  validates :name, :description, :location, :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id, class_name: 'Event'
+  has_many :invitations, foreign_key: :attendee
+  has_many :attended_events, through: :invitations, source: :attended_event
+
   validates :name, presence: true, uniqueness: { case_sensitive: false },
                    length: { minimum: 4, maximum: 25 }
 end
