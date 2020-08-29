bc02decf3fc13f5bf336db8df5806b9b8f0e8bb6
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-29 22:03:00 +0530

    Add nav-bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(name: params[:session][:user] 
-  		|| User.find(params[:session][:user].to_i)
+  	@user = User.find_by(name: params[:session][:user])
+  	@user ||= User.find(params[:session][:user].to_i)
 
   	if @user 
           session[:user_id] = @user.id
           session[:username] = @user.name
           redirect_to user_path(@user)
-      else
+    else
         flash.now[:danger] = "Something wrong..."
         render 'new'
-      end  
+    end  
   end
 
   def destroy
