d3445fbc32e758cdebd2a5537e23f2c70c2862b3
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-30 05:39:49 +0530

    Add User testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'Deepesh')
+  end
+
+  test 'User should be valid' do
+    assert @user.valid?
+  end
+
+  test 'user name should be present' do
+    @user.name = ' '
+    assert_not @user.valid?
+  end
+
+  test 'user name should be unique' do
+    @user.save
+    @user2 = User.new(name: 'Deepesh')
+    assert_not @user2.valid?
+  end
   # test "the truth" do
   #   assert true
   # end
