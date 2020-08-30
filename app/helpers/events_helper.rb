8df89ad86883639726f7ca883672cfdfa22a798f
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-31 01:44:02 +0530

    Add logic in event helper instead of events view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,11 +1,17 @@
 module EventsHelper
   def doevents(name, events)
-    render partial: name, locals: { obj: events}
+    render partial: name, locals: { obj: events }
   end
 
   def alluser(users)
-  	users.each do |user|
-  	  content_tag(:p, (link_to user.name, user_path(user)))
-  	end
+    users.each do |user|
+      concat(content_tag(:hr) + content_tag(:p, (link_to user.name, user_path(user))))
+    end
+  end
+
+  def event_attendee(event)
+    event.attendees.each do |event|
+      concat(content_tag(:hr) + content_tag(:p, event.name))
+    end
   end
 end
